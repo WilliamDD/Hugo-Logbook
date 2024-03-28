3e832122d842e9132fc5511619cd55ceadc62bd7
William <william@dedooij.eu>
2024-03-28 21:26:35 +0100

    removed bilibili and slick

--- static/js/script.js
+++ static/js/script.js
@@ -81,14 +81,14 @@
 	});
 
 	//post slider
-	$('.post-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		dots: false,
-		arrows: true,
-		prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
-		nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>'
-	});
+	// $('.post-slider').slick({
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	autoplay: true,
+	// 	dots: false,
+	// 	arrows: true,
+	// 	prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
+	// 	nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>'
+	// });
 
 })(jQuery);
